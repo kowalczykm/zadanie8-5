b5e8675c806d62d68e4611297d2d8b9e73f54bed
marek <mkrzkowalczyk@gmail.com>
2017-06-30 14:23:49 +0200

    triangle

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// scripts.js
+/* plik javascript*/
+var a = prompt('Enter a');
+alert('a = ' + a);
+var h = prompt('Enter h');
+alert('h = ' + h);
+var triangleArea = a * h / 2;
+alert('Pole trójkąta = ' + triangleArea);
+// alert('Hello, ' + name);
+// konsola
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
